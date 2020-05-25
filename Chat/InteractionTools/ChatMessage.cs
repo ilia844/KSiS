8ea33b04b8bc9c6f94001b9ada8319a010f8d615
ilia844 <w5332660@mail.ru>
2020-05-26 00:27:24 +0300

    Init and created solution structure

---
+++ Chat/InteractionTools/ChatMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractionTools
+{
+    [Serializable]
+    public class ChatMessage
+    {
+        public int SenderId;
+        public string SenderName;
+        public string Content;
+        public DateTime Time;
+
+        public ChatMessage(int senderId, string senderName, string content, DateTime time)
+        {
+            SenderId = senderId;
+            SenderName = senderName;
+            Content = content;
+            Time = time;
+        }
+
+        public ChatMessage()
+        {
+
+        }
+    }
+}
